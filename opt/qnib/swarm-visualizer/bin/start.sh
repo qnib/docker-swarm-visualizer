4dfcefbc827e56aa0e7f74509aaf990275e2e771
Christian Kniep <christian@qnib.org>
2016-09-01 17:11:43 +0200

    adjust DOCKER_HOST if necessary

--- opt/qnib/swarm-visualizer/bin/start.sh
+++ opt/qnib/swarm-visualizer/bin/start.sh
@@ -1,4 +1,7 @@
 #!/usr/local/bin/dumb-init /bin/bash
 
 export HOST=${ADV_HOST:-localhost}
+if [ "X$(echo ${DOCKER_HOST} |egrep -o "tcp://")" != "X" ];then
+   export DOCKER_HOST=$(echo ${DOCKER_HOST} |sed 's#tcp://##')
+fi
 npm start
